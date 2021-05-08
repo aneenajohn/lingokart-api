26732d4984d6f6649d59797e1ff5e9901908473d
aneenajohn <75213145+aneenajohn@users.noreply.github.com>
2021-05-08 09:47:32 +0530

    final commit

--- middlewares/error-handler.middleware.js
+++ middlewares/error-handler.middleware.js
@@ -1,7 +1,6 @@
 
 const errorHandler = (err, req, res, next) => {
   console.error(err.stack)
-  // res.status(500).send('Something broke!')
   res.status(500).json(
     {success:false,error:err.message,message:"Err occured!Have a look at err stack"});
 }
